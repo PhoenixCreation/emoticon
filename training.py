8f983511bb7358af06d5fa88a2fa2590e5408bd0
phoenix <phs28052001@gmail.com>
2021-09-10 13:21:01 +0530

    🏃‍♂️📜 Training model, Adding logs.

--- training.py
+++ training.py
@@ -1,5 +1,16 @@
 import numpy as np
 import tensorflow as tf
+
+import keras
+
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D
+from keras.layers import Dense, Activation, Dropout, Flatten
+
+
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+
 import datetime
 
 
@@ -37,8 +48,7 @@ print_main("Starting Programme")
 
 # Load data
 print_log("Loading data started")
-# data_file = "data/fer2013.csv"
-data_file = "data/temp.csv"
+data_file = "data/fer2013.csv"
 with open(data_file) as f:
     content = f.readlines()
 
@@ -98,5 +108,57 @@ print_info(f'Train samples: {x_train.shape[0]}')
 print_info(f'Test samples: {x_test.shape[0]}')
 print_success("[Pre-Processing] Done")
 
+print_log("Starting training model")
+
+print_log("[Training] Initializing Sequential model")
+# construct CNN structure
+model = Sequential()
+
+print_log("[Training] Adding convolution layers to model x 3")
+# 1st convolution layer
+model.add(Conv2D(64, (5, 5), activation='relu', input_shape=(48, 48, 1)))
+model.add(MaxPooling2D(pool_size=(5, 5), strides=(2, 2)))
+
+# 2nd convolution layer
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(AveragePooling2D(pool_size=(3, 3), strides=(2, 2)))
+
+# 3rd convolution layer
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(AveragePooling2D(pool_size=(3, 3), strides=(2, 2)))
+
+model.add(Flatten())
+
+# fully connected neural networks
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.2))
+
+model.add(Dense(num_classes, activation='softmax'))
+
+print_log("[Training] Generating image data in batches")
+gen = ImageDataGenerator()
+batch_size = 128
+train_generator = gen.flow(x_train, y_train, batch_size=batch_size)
+
+print_log("[Training] configuring model for training")
+model.compile(loss='categorical_crossentropy',
+              optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+
+epochs = 7
+print_log("[Training] Fitting the model as per batches")
+model.fit_generator(train_generator, steps_per_epoch=batch_size, epochs=epochs)
+
+print_log("[Training] Done.")
+train_score = model.evaluate(x_train, y_train, verbose=0)
+print_info(f'Train loss: {train_score[0]}')
+print_info(f'Train accuracy: {100*train_score[1]}')
+
+test_score = model.evaluate(x_test, y_test, verbose=0)
+print_info(f'Test loss: {test_score[0]}')
+print_info(f'Test accuracy: {100*test_score[1]}')
 
 print_main("Ending Programme", False)
